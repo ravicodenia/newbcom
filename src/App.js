20215c7833f59f4ef1fc89b604519b371ef160be
ravicodenia <ravisrivastava@Ravis-MacBook-Air.local>
2024-05-04 14:15:05 +0530

    new

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ import GlobalFooter from "./components/footer/globalFooter";
 
 
 import UserD from "./components/Userside/UserD";
+import Home from "./components/Home/App";
 
 const Routing = () => {
 	const user = localStorage.getItem("token");
@@ -36,11 +37,14 @@ const Routing = () => {
 			<Route path="/login" exact element={<Login />} />
 			// <Route path="/signup" exact element={<Signup />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
+			<Route path="/home" exact element={<Home />} />
 
 			{user && <Route path="/partners" exact element={<Partners />} />}
 			{user && <Route path="/partners/add-partnerform" exact element={<Addpartnerform />} />}
 			{user && <Route path="/partners/partnerdetails/:val" exact element={<Partnerdetails />} />}
 			{user && <Route path="/partners/companydetails" exact element={<Companydetails />} />}
+
+
 
 			{user1 && <Route path="/landingpage/assessments" exact element={<Userassessments />} />}
 			{<Route path="/landingpage/:val" exact element={<Userside />} />}
---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const homePage = () => {
+  
+
+  return (
+    <>
+
+
+
+    </>
+  );
+};
+
+export default homePage;
